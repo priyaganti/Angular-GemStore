d26f551dc154a22aaa3f7e9bcbde66a3c7a231c6
priyaganti <ganti.priya@gmail.com>
2017-03-05 14:01:21 -0800

    add a Controller,set product as its property and access the data from HTML

--- app.js
+++ app.js
@@ -1,1 +1,14 @@
-var app = angular.module('gemStore', []); 
+(function(){ //good practice to wrap the javascript inside a closure.
+  var app = angular.module('gemStore', []);
+
+  app.controller('storeController', function(){
+    this.product = gem;
+  });
+
+  var gem = {
+    name: 'Dodecahedron',
+    price: 2.95,
+    description : ''
+  };
+
+})();
